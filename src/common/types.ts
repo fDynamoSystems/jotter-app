454174e6a25597a5876917b596e77334bec317fb
fDynamo <fontaine.dynamo@gmail.com>
2023-11-25 00:29:02 -0800

    Added size type

--- src/common/types.ts
+++ src/common/types.ts
@@ -3,6 +3,7 @@ export type KeyboardModifiersState = {
   metaKey: boolean;
 };
 export type Coordinates = { x: number; y: number };
+export type Size = { width: number; height: number };
 export type NoteEditInfo = {
   content: string;
   filepath: string;
